ea76599fda84bbaa6db503f1da0918839a108c63
Izzana <izzana.martins@gmail.com>
2022-09-13 01:49:30 -0300

    versio 2

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var Play = 1;
 var End = 0;
 var gameState = Play;//tinha chamado a variável PLAY, mas ela se chama Play
 var imageFundo, imagemFundo;//criei uma nova variável para a imagem e outra pra o sprite, pq não podemos colocar as duas juntas
-var mario ;
+var mario, marioImg;
 var gameOver,restart;//criei uma nova variável para a imagem e outra pra o sprite, pq não podemos colocar as duas juntas
 var ground; //precisamos de um chão invisível para o sprite não cair no além kkkk
 var gameOverImg;
@@ -11,7 +11,7 @@ var score = 0;//a pontuação inicial tem que omeçar com 0
 
 function preload(){
   imagemFundo = loadImage("fundodejogo.jpg");//loadimage estava com i minúsculo
-  mario = loadAnimation("mario.png");//loadimage estava com i minúsculo
+  marioImg = loadAnimation("mario.png");//loadimage estava com i minúsculo
   gameOverImg = loadImage("gameOver.png");
   restart = loadImage("restart.png");
 }
@@ -22,9 +22,9 @@ function setup() {
     imageFundo.addImage("imageFundo",imagemFundo);//tinha chamado a variável errada
     imageFundo.x = imageFundo.width /2;//tinha chamado a variável errada, faltando um n
 
-    nave = createSprite(50,height-80,20,50);//estava escrito createspite
-    nave.addAnimation('jogador', mario);
-    nave.scale = 0.1;//estava score
+    mario = createSprite(50,height-80,20,50);//estava escrito createspite
+    mario.addAnimation('jogador', marioImg);
+    mario.scale = 0.1;//estava score
 
     gameOver = createSprite(width/2,height/2);
     gameOver.addImage(gameOverImg);//chamou uma variável que ainda não existia
@@ -46,29 +46,25 @@ function draw() {
       
       imageFundo.velocityX = -5;
       score = score + Math.round(getFrameRate()/60);
-      nave.velocityY = nave.velocityY + 0.8; //era velocityY para adicionar gravidade, e teu sprite em movimento se chama nave
-      if(keyDown('space') && nave.y >= 320){//o k estava maiúsculo e faltava mario.y, estava só y
-        nave.velocityY = -12;//faltava o Y
+      mario.velocityY = mario.velocityY + 0.8; //era velocityY para adicionar gravidade, e teu sprite em movimento se chama mario
+      if(keyDown('space') && mario.y >= 320){//o k estava maiúsculo e faltava mario.y, estava só y
+        mario.velocityY = -12;//faltava o Y
       }
 
       if(imageFundo.x < 200){
           imageFundo.x = imageFundo.width/2;
       } 
-      nave.collide(ground)
+      mario.collide(ground)
+      /*
       if(obstaculo.isTouching(mario)){
         gameState = End;
       }
+      */
     } else if (gameState === End) {//estava escrito END
         gameOver.visible = true;
         restart.visible = true;
         imageFundo.velocityX = 0;
         mario.velocityY = 0
-      /*
-      if(mousePressedOver(restart)){
-        console.log("reiniciar")
-          reiniciar();
-      }
-      */
     }
 
   
